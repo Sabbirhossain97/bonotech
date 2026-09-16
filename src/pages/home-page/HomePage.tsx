0b82655b3a4fa39e75454f4331ca08dc4d7dab7a
Sakibur Rahman <sakib.cse.333@gmail.com>
2026-09-16 11:46:19 +0600

    Remove commented lines.

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,49 +1,37 @@
-// import { AnnouncementBar } from '@/components/sections/AnnouncementBar/AnnouncementBar'
-import { Hero } from '@/components/sections/Hero/Hero'
-import { Clients } from '@/components/sections/Clients/Clients'
-// import { Introduction } from '@/components/sections/Introduction/Introduction'
-import { IntroductionSection } from '@/components/sections/IntroductionSection/IntroductionSection'
-import { WhatWeDo } from '@/components/sections/WhatWeDo/WhatWeDo'
 import { AiApproach } from '@/components/sections/AiApproach/AiApproach'
-import { Comparison } from '@/components/sections/Comparison/Comparison'
-// import { Timeline } from '@/components/sections/Timeline/Timeline'
-import { TimelineSection } from '@/components/sections/TimelineSection/TimelineSection'
-// import { Speed } from '@/components/sections/Speed/Speed'
-import { SpeedSection } from '@/components/sections/SpeedSection/SpeedSection'
-import { Projects } from '@/components/sections/Projects/Projects'
-import { Industries } from '@/components/sections/Industries/Industries'
-// import { BonoExperience } from '@/components/sections/BonoExperience/BonoExperience'
 import { BonoExperienceSection } from '@/components/sections/BonoExperienceSection/BonoExperienceSection'
-import { Testimonials } from '@/components/sections/Testimonials/Testimonials'
+import { Clients } from '@/components/sections/Clients/Clients'
+import { Comparison } from '@/components/sections/Comparison/Comparison'
 import { FAQ } from '@/components/sections/FAQ/FAQ'
-import { Contact } from '@/components/sections/Contact/Contact'
-import { Scheduling } from '@/components/sections/Scheduling/Scheduling'
 import { Footer } from '@/components/sections/Footer/Footer'
+import { Hero } from '@/components/sections/Hero/Hero'
+import { Industries } from '@/components/sections/Industries/Industries'
+import { IntroductionSection } from '@/components/sections/IntroductionSection/IntroductionSection'
 import { Navbar } from './components/Navbar'
+import { Projects } from '@/components/sections/Projects/Projects'
+import { Scheduling } from '@/components/sections/Scheduling/Scheduling'
+import { SpeedSection } from '@/components/sections/SpeedSection/SpeedSection'
+import { Testimonials } from '@/components/sections/Testimonials/Testimonials'
+import { TimelineSection } from '@/components/sections/TimelineSection/TimelineSection'
+import { WhatWeDo } from '@/components/sections/WhatWeDo/WhatWeDo'
 
 export function HomePage() {
     return (
         <>
-            {/* <AnnouncementBar /> */}
             <Navbar />
             <Hero />
             <Clients />
-            {/* <Introduction /> */}
             <IntroductionSection />
             <Comparison />
             <WhatWeDo />
             <AiApproach />
-            {/* <Timeline /> */}
             <TimelineSection />
-            {/* <Speed /> */}
             <SpeedSection />
             <Projects />
             <Industries />
-            {/* <BonoExperience /> */}
             <BonoExperienceSection />
             <Testimonials />
             <FAQ />
-            {/* <Contact /> */}
             <Scheduling />
             <Footer />
         </>
